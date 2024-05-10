38cf11b11ac43b5dae9e12b6952570f1faf4f3f7
dfghjuy6 <js1meweda07@hotmail.com>
2024-05-10 10:08:01 +0700

    demo project commit

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-const insertionSort = (arr) => {
-  for (let i = 1; i < arr.length; i++) {
-    let key = arr[i];
-    let j = i - 1;
-    while (j >= 0 && arr[j] > key) {
-      arr[j + 1] = arr[j];
-      j--;
+function countAndSay(n) {
+  let result = "1";
+  for (let i = 1; i < n; i++) {
+    let temp = "";
+    let count = 1;
+    for (let j = 0; j < result.length; j++) {
+      if (result[j] === result[j + 1]) {
+        count++;
+      } else {
+        temp += count + result[j];
+        count = 1;
+      }
     }
-    arr[j + 1] = key;
+    result = temp;
   }
-  return arr;
-};
+  return result;
+}
