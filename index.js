e1fe09f32f9964d11ab087ea583b7103c45e92d4
dfghjuy6 <js1meweda07@hotmail.com>
2024-05-11 22:03:43 +0700

    test task

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
-function countAndSay(n) {
-  let result = "1";
-  for (let i = 1; i < n; i++) {
-    let temp = "";
-    let count = 1;
-    for (let j = 0; j < result.length; j++) {
-      if (result[j] === result[j + 1]) {
-        count++;
-      } else {
-        temp += count + result[j];
-        count = 1;
-      }
-    }
-    result = temp;
+function hasCycle(head) {
+  let slow = head;
+  let fast = head;
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+    if (slow === fast) return true;
   }
-  return result;
+  return false;
 }
